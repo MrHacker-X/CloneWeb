c1ae19eadb84001c4785337b0a06cf706f9f9b68
MrHacker-X <70113933+MrHacker-X@users.noreply.github.com>
2021-04-19 15:52:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ chmod +x /data/data/com.termux/files/usr/bin/clone
 chmod +x /data/data/com.termux/files/usr/share/CloneWeb/clone.sh
 echo
 echo -e "\033[1;92mSetup is completed simply type command\033[1;97m clone \033[1;92mAny time in your terminal for launch script"
-rm -rf CloneWeb
+
